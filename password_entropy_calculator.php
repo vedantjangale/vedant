1e2799c7cf4ab94ace87222e860edb79c55fb2a4
vedantjangale <153490983+vedantjangale@users.noreply.github.com>
2023-12-12 14:16:05 +0530

    Update password_entropy_calculator.php

--- password_entropy_calculator.php
+++ password_entropy_calculator.php
@@ -1,26 +1,21 @@
 <?php
-
-function calculateEntropy($password) {
-    $charsetSize = 72; // Size of character set (lowercase + uppercase + digits + special characters)
-    $passwordLength = strlen($password);
-
-    // Calculate entropy using the formula: log2(charsetSize) * passwordLength
-    $entropy = log($charsetSize, 2) * $passwordLength;
-
-    return $entropy;
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     if (!empty($password)) {
-        $entropy = calculateEntropy($password);
-        echo "Password Entropy: " . $entropy;
+        $charsetSize = 72; // Size of character set (lowercase + uppercase + digits + special characters)
+        $passwordLength = strlen($password);
+
+        // Calculate entropy using the formula: log2(charsetSize) * passwordLength
+        $entropy = log($charsetSize, 2) * $passwordLength;
+
+        echo "Password Entropy: " . number_format($entropy, 2);
     } else {
         echo "Please enter a password.";
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,13 +23,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Entropy Calculator</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+    </style>
 </head>
 <body>
+
     <h1>Password Entropy Calculator</h1>
-    <form method="post">
-        <label for="password">Enter Password:</label>
-        <input type="password" name="password" required>
-        <button type="submit">Calculate Entropy</button>
-    </form>
+    
+    <label for="password">Enter Password:</label>
+    <input type="password" id="password" oninput="calculateEntropy()">
+    
+    <p id="entropy-display">Password Entropy: <span id="entropy-value">0</span></p>
+
+    <script>
+        function calculateEntropy() {
+            var password = document.getElementById("password").value;
+            var charsetSize = 72; // Size of character set (lowercase + uppercase + digits + special characters)
+            var passwordLength = password.length;
+
+            // Calculate entropy using the formula: log2(charsetSize) * passwordLength
+            var entropy = Math.log2(charsetSize) * passwordLength;
+
+            document.getElementById("entropy-value").textContent = entropy.toFixed(2);
+        }
+    </script>
+
 </body>
 </html>
